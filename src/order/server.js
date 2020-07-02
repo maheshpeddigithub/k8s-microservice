c32e671695e3951ad95c238e1a4727e76c8d1bc2
mylidluk <mylidluk@gmail.com>
2020-07-02 01:40:48 +0100

    first commit

---
+++ src/order/server.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const app = require("./app");
+
+app.listen(9000, () => console.log("server starting on port 9000!"));
---
+++ src/product/server.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const app = require("./app");
+
+app.listen(9091, () => console.log("server starting on port 9091!"));
